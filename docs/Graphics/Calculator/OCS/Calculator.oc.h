d390da0415af1670f33d2e482ed7397c9b6e272b
Daniel Norman <daniel@2color.me>
2013-12-30 13:26:29 +0100

    Added example from docs.

---
+++ docs/Graphics/Calculator/OCS/Calculator.oc.h
@@ -0,0 +1,119 @@
+
+
+#ifndef ACalculator_included
+#define ACalculator_included
+
+#define __ACalculator_Acalculator __ACalculator_1
+extern OBJ __ACalculator_Acalculator;
+
+#define __ACalculator_AcalcLogic __ACalculator_2
+#define _ACalculator_AcalcLogic _ACalculator_2
+extern OBJ __ACalculator_AcalcLogic;
+extern OBJ _ACalculator_AcalcLogic(OBJ,OBJ,OBJ,OBJ);
+
+#define __ACalculator_AkeyLabels __ACalculator_3
+extern OBJ __ACalculator_AkeyLabels;
+
+#define __ACalculator_AkeySyms __ACalculator_4
+extern OBJ __ACalculator_AkeySyms;
+
+#define __ACalculator_AcalcSize __ACalculator_5
+extern OBJ __ACalculator_AcalcSize;
+
+#define __ACalculator_AcalcMinSize __ACalculator_6
+extern OBJ __ACalculator_AcalcMinSize;
+
+#define __ACalculator_AcalcMaxSize __ACalculator_7
+extern OBJ __ACalculator_AcalcMaxSize;
+
+#define __ACalculator_Amatrix __ACalculator_8
+#define _ACalculator_Amatrix _ACalculator_8
+extern OBJ __ACalculator_Amatrix;
+extern OBJ _ACalculator_Amatrix(OBJ,OBJ);
+
+#define __ACalculator_AsplitSegm __ACalculator_9
+#define _ACalculator_AsplitSegm _ACalculator_9
+extern OBJ __ACalculator_AsplitSegm;
+extern OBJ _ACalculator_AsplitSegm(OBJ,OBJ);
+
+#ifndef ACalculator_AcalcLogic
+#define ACalculator_AcalcLogic(x1,x2,x3,x4,x7) {x7=_ACalculator_AcalcLogic(x1,x2,x3,x4);}
+#endif
+
+#ifndef ACalculator_Amatrix
+#define ACalculator_Amatrix(x1,x2,x5) {x5=_ACalculator_Amatrix(x1,x2);}
+#endif
+
+#ifndef ACalculator_AsplitSegm
+#define ACalculator_AsplitSegm(x1,x2,x8) {x8=_ACalculator_AsplitSegm(x1,x2);}
+#endif
+
+#ifndef ACalculator_Acalculator_L11
+#define ACalculator_Acalculator_L11(x1,x2,x6) {x6=_ACalculator_Acalculator_L11(x1,x2);}
+#endif
+
+#ifndef ACalculator_Acalculator_L12
+#define ACalculator_Acalculator_L12(x1,x2) {x2=_ACalculator_Acalculator_L12(x1);}
+#endif
+
+#ifndef ACalculator_Acalculator_L13
+#define ACalculator_Acalculator_L13(x1,x2,x3,x4) {x4=_ACalculator_Acalculator_L13(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_Acalculator_L14
+#define ACalculator_Acalculator_L14(x1,x2,x3,x6) {x6=_ACalculator_Acalculator_L14(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_Acalculator_L15
+#define ACalculator_Acalculator_L15(x1,x2,x3,x4,x8) {x8=_ACalculator_Acalculator_L15(x1,x2,x3,x4);}
+#endif
+
+#ifndef ACalculator_Acalculator_L16
+#define ACalculator_Acalculator_L16(x1,x2,x3,x6) {x6=_ACalculator_Acalculator_L16(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_Acalculator_L17
+#define ACalculator_Acalculator_L17(x1,x2,x20) {x20=_ACalculator_Acalculator_L17(x1,x2);}
+#endif
+
+#ifndef ACalculator_Acalculator_L18
+#define ACalculator_Acalculator_L18(x1,x3) {x3=_ACalculator_Acalculator_L18(x1);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L25
+#define ACalculator_AcalcLogic_L25(x1,x2,x3) {x3=_ACalculator_AcalcLogic_L25(x1,x2);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L26
+#define ACalculator_AcalcLogic_L26(x1,x2) {x2=_ACalculator_AcalcLogic_L26(x1);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L27
+#define ACalculator_AcalcLogic_L27(x1,x2,x3,x5) {x5=_ACalculator_AcalcLogic_L27(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L28
+#define ACalculator_AcalcLogic_L28(x1,x2,x3,x5) {x5=_ACalculator_AcalcLogic_L28(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L29
+#define ACalculator_AcalcLogic_L29(x1,x2,x3,x5) {x5=_ACalculator_AcalcLogic_L29(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L30
+#define ACalculator_AcalcLogic_L30(x1,x2,x3,x5) {x5=_ACalculator_AcalcLogic_L30(x1,x2,x3);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L31
+#define ACalculator_AcalcLogic_L31(x1,x2,x3,x4,x5,x6) {x6=_ACalculator_AcalcLogic_L31(x1,x2,x3,x4,x5);}
+#endif
+
+#ifndef ACalculator_AcalcLogic_L32
+#define ACalculator_AcalcLogic_L32(x1,x2,x3,x4,x5,x28) {x28=_ACalculator_AcalcLogic_L32(x1,x2,x3,x4,x5);}
+#endif
+
+#ifndef ACalculator_Amatrix_L103
+#define ACalculator_Amatrix_L103(x1,x2) {x2=_ACalculator_Amatrix_L103(x1);}
+#endif
+
+#endif
