a130ee5c1244437c3566c93f1c943d0f7a6bc7c0
Daniel Norman <daniel@2color.me>
2013-12-30 13:27:57 +0100

    Viewer and Settings modules. Initial commit.

---
+++ OCS/Viewer.oc.h
@@ -0,0 +1,159 @@
+
+
+#ifndef AViewer_included
+#define AViewer_included
+
+#define __AViewer_Aviewer __AViewer_1
+extern OBJ __AViewer_Aviewer;
+
+#define __AViewer_Aexit __AViewer_3
+extern OBJ __AViewer_Aexit;
+
+#define __AViewer_Aexit_ __AViewer_4
+#define _AViewer_Aexit_ _AViewer_4
+extern OBJ __AViewer_Aexit_;
+extern OBJ _AViewer_Aexit_(OBJ);
+
+#define __AViewer_Agui __AViewer_6
+#define _AViewer_Agui _AViewer_6
+extern OBJ __AViewer_Agui;
+extern OBJ _AViewer_Agui(OBJ,OBJ,OBJ,OBJ);
+
+#define __AViewer_Agui_ __AViewer_7
+#define _AViewer_Agui_ _AViewer_7
+extern OBJ __AViewer_Agui_;
+extern OBJ _AViewer_Agui_(OBJ);
+
+#define __AViewer_Ametr __AViewer_8
+#define _AViewer_Ametr _AViewer_8
+extern OBJ __AViewer_Ametr;
+extern OBJ _AViewer_Ametr(OBJ);
+
+#define __AViewer_Atitle __AViewer_9
+#define _AViewer_Atitle _AViewer_9
+extern OBJ __AViewer_Atitle;
+extern OBJ _AViewer_Atitle(OBJ);
+
+#define __AViewer_Aemit __AViewer_10
+#define _AViewer_Aemit _AViewer_10
+extern OBJ __AViewer_Aemit;
+extern OBJ _AViewer_Aemit(OBJ);
+
+#define __AViewer_Aedit __AViewer_11
+#define _AViewer_Aedit _AViewer_11
+extern OBJ __AViewer_Aedit;
+extern OBJ _AViewer_Aedit(OBJ);
+
+#define __AViewer_AsetupGui __AViewer_12
+extern OBJ __AViewer_AsetupGui;
+
+#define __AViewer_AsetupWindow __AViewer_13
+#define _AViewer_AsetupWindow _AViewer_13
+extern OBJ __AViewer_AsetupWindow;
+extern OBJ _AViewer_AsetupWindow(OBJ);
+
+#define __AViewer_Aframe __AViewer_14
+#define _AViewer_Aframe _AViewer_14
+extern OBJ __AViewer_Aframe;
+extern OBJ _AViewer_Aframe(OBJ);
+
+#define __AViewer_Apad __AViewer_15
+#define _AViewer_Apad _AViewer_15
+extern OBJ __AViewer_Apad;
+extern OBJ _AViewer_Apad(OBJ);
+
+#define __AViewer_AexitButton __AViewer_16
+#define _AViewer_AexitButton _AViewer_16
+extern OBJ __AViewer_AexitButton;
+extern OBJ _AViewer_AexitButton(OBJ);
+
+#define __AViewer_AwaitForTermination __AViewer_17
+#define _AViewer_AwaitForTermination _AViewer_17
+extern OBJ __AViewer_AwaitForTermination;
+extern OBJ _AViewer_AwaitForTermination(OBJ,OBJ);
+
+#ifndef AViewer_Aexit_
+#define AViewer_Aexit_(x1,x2) {x2=_AViewer_Aexit_(x1);}
+#endif
+
+#ifndef AViewer_Agui
+#define AViewer_Agui(x1,x2,x3,x4,x5) {x5=_AViewer_Agui(x1,x2,x3,x4);}
+#endif
+
+#ifndef AViewer_Agui_
+#define AViewer_Agui_(x1,x2) {x2=_AViewer_Agui_(x1);}
+#endif
+
+#ifndef AViewer_Ametr
+#define AViewer_Ametr(x1,x6) {x6=_AViewer_Ametr(x1);}
+#endif
+
+#ifndef AViewer_Atitle
+#define AViewer_Atitle(x1,x6) {x6=_AViewer_Atitle(x1);}
+#endif
+
+#ifndef AViewer_Aemit
+#define AViewer_Aemit(x1,x6) {x6=_AViewer_Aemit(x1);}
+#endif
+
+#ifndef AViewer_Aedit
+#define AViewer_Aedit(x1,x6) {x6=_AViewer_Aedit(x1);}
+#endif
+
+#ifndef AViewer_AsetupWindow
+#define AViewer_AsetupWindow(x1,x5) {x5=_AViewer_AsetupWindow(x1);}
+#endif
+
+#ifndef AViewer_Aframe
+#define AViewer_Aframe(x1,x6) {x6=_AViewer_Aframe(x1);}
+#endif
+
+#ifndef AViewer_Apad
+#define AViewer_Apad(x1,x7) {x7=_AViewer_Apad(x1);}
+#endif
+
+#ifndef AViewer_AexitButton
+#define AViewer_AexitButton(x1,x5) {x5=_AViewer_AexitButton(x1);}
+#endif
+
+#ifndef AViewer_AwaitForTermination
+#define AViewer_AwaitForTermination(x1,x2,x6) {x6=_AViewer_AwaitForTermination(x1,x2);}
+#endif
+
+#ifndef AViewer_Aviewer_L18
+#define AViewer_Aviewer_L18(x1,x2,x3) {x3=_AViewer_Aviewer_L18(x1,x2);}
+#endif
+
+#ifndef AViewer_Aviewer_L19
+#define AViewer_Aviewer_L19(x1,x4) {x4=_AViewer_Aviewer_L19(x1);}
+#endif
+
+#ifndef AViewer_AsetupGui_L21
+#define AViewer_AsetupGui_L21(x1,x2,x3,x5) {x5=_AViewer_AsetupGui_L21(x1,x2,x3);}
+#endif
+
+#ifndef AViewer_AsetupGui_L22
+#define AViewer_AsetupGui_L22(x1,x2,x5) {x5=_AViewer_AsetupGui_L22(x1,x2);}
+#endif
+
+#ifndef AViewer_AsetupGui_L23
+#define AViewer_AsetupGui_L23(x1,x3) {x3=_AViewer_AsetupGui_L23(x1);}
+#endif
+
+#ifndef AViewer_AsetupWindow_L24
+#define AViewer_AsetupWindow_L24(x1,x2,x3) {x3=_AViewer_AsetupWindow_L24(x1,x2);}
+#endif
+
+#ifndef AViewer_AsetupWindow_L25
+#define AViewer_AsetupWindow_L25(x1,x2,x5) {x5=_AViewer_AsetupWindow_L25(x1,x2);}
+#endif
+
+#ifndef AViewer_AsetupWindow_L26
+#define AViewer_AsetupWindow_L26(x1,x2,x7) {x7=_AViewer_AsetupWindow_L26(x1,x2);}
+#endif
+
+#ifndef AViewer_AwaitForTermination_L28
+#define AViewer_AwaitForTermination_L28(x1,x2,x8) {x8=_AViewer_AwaitForTermination_L28(x1,x2);}
+#endif
+
+#endif
